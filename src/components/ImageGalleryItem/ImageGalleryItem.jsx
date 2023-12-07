5adf5f5552d4733207c29d8671880794de123ecd
Viktoriya <arizona081392@gmail.com>
2023-12-07 15:53:52 +0200

    ok1

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,13 +1,15 @@
 import css from './ImageGallery.module.css'
-export const ImageGalleryItem = ({ image, onClick }) => {
+export const ImageGalleryItem = ({ images, onClick }) => {
   return (
    
-      <li className={css.li}>
+    <li className={css.li}>
+      {images.map(image => (
          <img className= {css.img} src={image.webformatURL}
         alt={image.tags}
           id={image.id}
               onClick={() => onClick(image.webformatURL)}
-          />
+        />
+              ))}
 </li>
   );
 };
