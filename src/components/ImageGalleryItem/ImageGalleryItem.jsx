1c6d35be6be776479712fd36dc7e50b18db6450f
Viktoriya <arizona081392@gmail.com>
2023-12-07 16:11:51 +0200

    ok

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,15 +1,15 @@
 import css from './ImageGallery.module.css'
-export const ImageGalleryItem = ({ images, onClick }) => {
+export const ImageGalleryItem = ({ image, onClick }) => {
   return (
    
     <li className={css.li}>
-      {images.map(image => (
+      
          <img className= {css.img} src={image.webformatURL}
         alt={image.tags}
           id={image.id}
               onClick={() => onClick(image.webformatURL)}
         />
-              ))}
+              
 </li>
   );
 };
